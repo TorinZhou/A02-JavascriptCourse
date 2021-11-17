33a2eafab8cc9ea1fd39f777624995532fb44c5f
Torin Zhou <torinzhou@gmail.com>
2021-11-18 06:15:48 +0800

    11.17

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -1,8 +1,18 @@
-let js = 'amazing';
-console.log(33 + 33 + 33);
+// CHALLENGE 1 
+// Store mass and height
+const marksWeight = 78,
+marksHeight = 1.69,
+johnsWeight = 92,
+johnsHeight = 1.95;
+// Calculat BMI
+let marksBMI = marksWeight / marksHeight ** 2;
+let johnsBMI = johnsWeight / johnsHeight ** 2;
+// mark higher BMI
+let markHigherBMI = marksBMI > johnsBMI;
+console.log(markHigherBMI);
 
-//  Variables   (store)  Values
-console.log('Torin');
-let firstName = 'Torin';
-console.log(firstName);
-let PI = 3.14;
+if(markHigherBMI) {
+  console.log(`Mark's BMI ${marksBMI} is higher than John's ${johnsBMI}`)
+} else {  
+  console.log(`John's BMI ${johnsBMI} is higher than Mark's ${marksBMI}`)
+}
