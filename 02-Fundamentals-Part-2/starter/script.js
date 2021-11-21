1a573bc778d03dd9545b336b26133470ed4882b1
Torin Zhou <torinzhou@gmail.com>
2021-11-21 23:38:06 +0800

    startDOM

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -1,35 +1,33 @@
 // this has to be at the very beginning
 `use strict`;
 
-
 // //chalange 1
 // const calcAverage = (score1, score2, score3) => (score1 + score2 + score3)/3;
 
 // let calcAverage = function (score1, score2, score3) {
-  //   age = (score1 + score2 + score3)/3;
-  //   return age;
-  // };
-  // let calcAverage = function (score1, score2, score3) {
-    //   return (score1 + score2 + score3)/3;
-    // };
-    
-    // alert(calcAverage(1, 1, 9));
-    
-    // const checkWinner = function(dolphinScore1, dolphinScore2, dolphinScore3, koalaScore1, koalaScore2, koalaScore3) {
-    //   const avgDolphins = calcAverage(dolphinScore1, dolphinScore2, dolphinScore3);
-    //   const avgKoalas = calcAverage(koalaScore1, koalaScore2, koalaScore3);
-    //   const winner = (avgDolphins >= 2 * avgKoalas) ? 
-    //   `winner is Dolpins and vitory poins is ${avgDolphins}` 
-    //   : (avgKoalas >= 2 * avgDolphins) ? 
-    //   `winner is Koalas and the vitory point is ${avgKoalas}` 
-    //   : "nobody win" ; 
-    //   console.log(winner);
-    // } 
-    
-    
+//   age = (score1 + score2 + score3)/3;
+//   return age;
+// };
+// let calcAverage = function (score1, score2, score3) {
+//   return (score1 + score2 + score3)/3;
+// };
+
+// alert(calcAverage(1, 1, 9));
+
+// const checkWinner = function(dolphinScore1, dolphinScore2, dolphinScore3, koalaScore1, koalaScore2, koalaScore3) {
+//   const avgDolphins = calcAverage(dolphinScore1, dolphinScore2, dolphinScore3);
+//   const avgKoalas = calcAverage(koalaScore1, koalaScore2, koalaScore3);
+//   const winner = (avgDolphins >= 2 * avgKoalas) ?
+//   `winner is Dolpins and vitory poins is ${avgDolphins}`
+//   : (avgKoalas >= 2 * avgDolphins) ?
+//   `winner is Koalas and the vitory point is ${avgKoalas}`
+//   : "nobody win" ;
+//   console.log(winner);
+// }
+
 // //chalange 1
 // const calctip = function (bill) {
-//   const tip = (bill >= 50 && bill <= 300) ? 
+//   const tip = (bill >= 50 && bill <= 300) ?
 //   0.15 * bill : 0.2 * bill;
 //   return tip;
 // }
@@ -42,7 +40,6 @@
 // const total = [bills[0] + tips[0], bills[1] + tips[1], bills[2] + tips[2]];
 // console.log(total);
 
-
 //object small challange
 // const jonas = {
 //   firstName: 'Jonas',
@@ -53,7 +50,6 @@
 // };
 
 // console.log(`${jonas.firstName} has ${jonas.friends.length} fiends, and his best friend is ${jonas.friends[0]}.`)
-
 
 //object small challange 2
 // const jonas = {
@@ -77,7 +73,7 @@
 //   calcAge: function() {return 2037 - this.birthYeah;},
 //   // calcAge: () => 2037 - this.birthYeah,
 //   getInfo: function(){return `${this.firstName} is a ${this.calcAge()}-year old ${this.job}, and he has ${this.hasDriversLicense ? 'a' : 'no' } driver license.`;},
-//   // getInfoString: `${jonas.firstName}`, 
+//   // getInfoString: `${jonas.firstName}`,
 // }
 // console.log(jonas.getInfo());
 // // It's jonas that calling getinfo, so this = jonas
@@ -87,7 +83,7 @@
 //   firstName : 'Michael',
 //   lastName : 'Jordan',
 //   getName : function(){return `${this.firstName} ${this.lastName}`;},
-//   getName2 :`${this.firstName} ${this.whatever} ${aPerson.lastName} `,  
+//   getName2 :`${this.firstName} ${this.whatever} ${aPerson.lastName} `,
 // }
 // console.log(aPerson.getName());
 // console.log(aPerson.getName2);
@@ -107,37 +103,35 @@
 
 // console.log(mark.calcBMI());
 // const compareBMI = function(name1,name2){
-//   const message = (name1.calcBMI() > name2.calcBMI()) ? 
+//   const message = (name1.calcBMI() > name2.calcBMI()) ?
 //   `${name1.fullName}'s BMI (${name1.calcBMI()}) is higher than ${name2.fullName}'s (${name2.calcBMI()})`:
-//   (name2.calcBMI() > name1.calcBMI()) ? 
+//   (name2.calcBMI() > name1.calcBMI()) ?
 //   `${name2.fullName}'s BMI (${name2.calcBMI()}) is higher than ${name1.fullName}'s (${name1.calcBMI()})`:
 //   `They have the same BMI`;
 //   return console.log(message);
 // }
 // compareBMI(mark,john);
 
-
 // challenge 4
 
-const bills = [22, 295, 176, 440, 37, 105, 10, 1100, 86, 52]; 
+const bills = [22, 295, 176, 440, 37, 105, 10, 1100, 86, 52];
 const tips = [];
 const totals = [];
 const calctip = function (bill) {
-  const tip = (bill >= 50 && bill <= 300) ? 
-  0.15 * bill : 0.2 * bill;
+  const tip = bill >= 50 && bill <= 300 ? 0.15 * bill : 0.2 * bill;
   return tip;
 };
-for (i=0; i < bills.length; i++ ) {
+for (i = 0; i < bills.length; i++) {
   tips.push(calctip(bills[i]));
   totals.push(bills[i] + calctip(bills[i]));
-};
-const calcAverage = function(arr) {
+}
+const calcAverage = function (arr) {
   let sum = 0;
-  for(i=0; i<arr.length; i++) {
+  for (i = 0; i < arr.length; i++) {
     sum += arr[i];
   }
-  return sum/arr.length;
-}
+  return sum / arr.length;
+};
 console.log(totals);
 console.log(calcAverage(totals));
 // calc the element average of a given array.
