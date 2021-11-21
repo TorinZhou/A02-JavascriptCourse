a059cdd0383f7f975710d3687740e155322e1e31
Torin Zhou <torinzhou@gmail.com>
2021-11-22 00:24:32 +0800

    dom2

--- 05-Guess-My-Number/starter/script.js
+++ 05-Guess-My-Number/starter/script.js
@@ -1,3 +1,14 @@
 'use strict';
 
-document.querySelector();
+// look what I get here  console.log(document.querySelector('.message'));
+// console.log(document.querySelector('.message'));
+// or document.querySelector('#message');
+// ------------------------------------
+// ------------------------------------
+// ------------------------------------
+// ------------------------------------
+console.log(document.querySelector('.message').textContent);
+// we select the element, and then from that element we select the property. which is textcontent.
+// ------------------------------------
+// ------------------------------------
+// ------------------------------------
