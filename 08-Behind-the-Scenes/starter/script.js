1c9dd9fdd3907eaa33f4e64dbd959cc686a5dbc2
Torin Zhou <torinzhou@gmail.com>
2021-11-27 06:33:28 +0800

    this

--- 08-Behind-the-Scenes/starter/script.js
+++ 08-Behind-the-Scenes/starter/script.js
@@ -1,1 +1,24 @@
 'use strict';
+const test = {
+  first: 1990,
+  second: 2021,
+  calcDuration: function () {
+    this.second - this.first;
+    console.log(this);
+  },
+};
+
+const wired = {
+  year: 2077,
+};
+wired.calcDuration = test.calcDuration;
+test.calcDuration();
+console.log(test, wired);
+
+wired.calcDuration();
+// this keyword always point to the object which calling the method.
+
+const test2 = test.calcDuration;
+
+test2();
+// in this case an error, test2 is just a regular function, not an object, this is undefined. this.year is undefined as well.
