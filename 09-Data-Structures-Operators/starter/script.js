8edddae5d3c6f03c3de3303512976f108bae0832
Torin Zhou <torinzhou@gmail.com>
2021-11-28 09:30:38 +0800

    Destructuring-Arr-Obj

--- 09-Data-Structures-Operators/starter/script.js
+++ 09-Data-Structures-Operators/starter/script.js
@@ -26,4 +26,60 @@ const restaurant = {
       close: 24,
     },
   },
+
+  orderDelivery: function ({ address = 'privet drive', mainIndex = '4' }) {
+    console.log(address, mainIndex);
+  },
 };
+
+// Destructuring will come in handy when dealing with API calls
+const { name, openingHours, categories } = restaurant;
+console.log(openingHours);
+
+// Change name
+const { name: rename, openingHours: hours } = restaurant;
+console.log(rename, hours);
+
+// defalut value
+// menu dose not even exist, set it with a  default of [], otherwise will get a undifine error.
+const { menu = [], starterMenu: starters = [] } = restaurant;
+console.log(menu, starters);
+
+// Mutating variables
+// In this case, a&b are already declared, we have to mutate them with a ().
+let a = 111;
+let b = 999;
+const obj = { a: 23, b: 7, c: 14 };
+// if simply write {a,b}= obj, will get a syntex error
+({ a, b } = obj);
+console.log(a, b);
+
+// Nested Objects
+// in case to retrieve Saturday
+const {
+  fri: { open: openDes = 0, close: closeDes = 0 }, // nested Destructuring with rename and a default value.
+} = openingHours;
+console.log(openDes);
+// Destructuring while pass arguments. !!!
+// we pass one obj, and get 2 vars in the funtion. (recieve the obj and do des immediately)
+restaurant.orderDelivery({
+  time: '22.30',
+  address: 'whatever',
+  mainIndex: 2,
+  starterIndex: 2,
+});
+// ------------------------------------
+// ------------------------------------
+// ------------------------------------
+// ------------------------------------
+// ------------------------------------
+// ------------------------------------
+// ------------------------------------
+// ------------------------------------
+const arr = [2, 3, 4];
+// const a = arr[0];
+// const b = arr[1];
+// const c = arr[2];
+const [x, y, z] = arr;
+console.log(x, y, z);
+// destructuring.
