fef774ec88f20cf6a4a7bc8d05d06d69d4eafed5
Torin Zhou <torinzhou@gmail.com>
2021-12-03 14:07:39 +0800

    12.3

--- 10-Functions/starter/script.js
+++ 10-Functions/starter/script.js
@@ -1,1 +1,22 @@
 'use strict';
+
+/* #################################
+Defalut parameters
+####################################*/
+
+const creatBooking = function (
+  flightNum,
+  numPassengers = 1,
+  price = numPassengers * 200
+) {
+  // ES5 way of default parameters
+  // numPassengers = numPassengers || 1;
+  // price = price || 199;
+  const bookingObj = {
+    flightNum,
+    numPassengers,
+    price,
+  };
+  console.log(bookingObj); // {flightNum: 'whatever', numPassengers: 1, price: 200}
+};
+creatBooking('whatever', undefined, undefined);
