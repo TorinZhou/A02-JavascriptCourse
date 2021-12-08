13e1bbbb7390b4be161ccd4473dfb601f1ef7151
Torin Zhou <torinzhou@gmail.com>
2021-12-08 16:12:40 +0800

    12.8 chllange 1.1 1.2

--- 10-Functions/starter/script.js
+++ 10-Functions/starter/script.js
@@ -89,80 +89,101 @@ Defalut parameters
 /* #################################
 6. The Call and aplly Method
 ####################################*/
-const lufthansa = {
-  airline: 'Lufthansa',
-  iataCode: 'LH',
-  bookings: [],
-  // book : function(flightNum) {
-  // (enhanced mothod iteral)}
-  book(flightNum, name) {
-    console.log(
-      `${name} booked a seat on ${this.airline} flight ${this.iataCode}${flightNum}`
-    );
-    this.bookings.push({
-      flight: `${this.iataCode}${flightNum}`,
-      name,
-    });
-  },
-};
-const eurowings = {
-  airline: 'Eurowings',
-  iataCode: 'EW',
-  bookings: [],
-};
-const swiss = {
-  airline: 'Swiss Aie Lines',
-  iataCode: 'LX',
-  bookings: [],
-};
-lufthansa.book(222, 'Torin');
-console.log(lufthansa.bookings);
+// const lufthansa = {
+//   airline: 'Lufthansa',
+//   iataCode: 'LH',
+//   bookings: [],
+//   // book : function(flightNum) {
+//   // (enhanced mothod iteral)}
+//   book(flightNum, name) {
+//     console.log(
+//       `${name} booked a seat on ${this.airline} flight ${this.iataCode}${flightNum}`
+//     );
+//     this.bookings.push({
+//       flight: `${this.iataCode}${flightNum}`,
+//       name,
+//     });
+//   },
+// };
+// const eurowings = {
+//   airline: 'Eurowings',
+//   iataCode: 'EW',
+//   bookings: [],
+// };
+// const swiss = {
+//   airline: 'Swiss Aie Lines',
+//   iataCode: 'LX',
+//   bookings: [],
+// };
+// lufthansa.book(222, 'Torin');
+// console.log(lufthansa.bookings);
 
-const book = lufthansa.book; //take the function out, new reference, in this case the "this keyword" inside of it will now point to undefined.
-book.call(lufthansa, 223, 'Torin'); // call method of functions
-// Torin booked a seat on Lufthansa flight LH223
-book.call(eurowings, 223, 'Torin');
-// Torin booked a seat on Eurowings flight EW223
+// const book = lufthansa.book; //take the function out, new reference, in this case the "this keyword" inside of it will now point to undefined.
+// book.call(lufthansa, 223, 'Torin'); // call method of functions
+// // Torin booked a seat on Lufthansa flight LH223
+// book.call(eurowings, 223, 'Torin');
+// // Torin booked a seat on Eurowings flight EW223
 
 /* #################################
 7. Bind
 ####################################*/
-// 1
-const bookEU = book.bind(eurowings);
-bookEU(24, 'Kobe Byrant');
-// Kobe Byrant booked a seat on Eurowings flight EW24
-const bookEU23 = book.bind(eurowings, 23);
-bookEU23('Torin');
-// Torin booked a seat on Eurowings flight EW23
+// // 1
+// const bookEU = book.bind(eurowings);
+// bookEU(24, 'Kobe Byrant');
+// // Kobe Byrant booked a seat on Eurowings flight EW24
+// const bookEU23 = book.bind(eurowings, 23);
+// bookEU23('Torin');
+// // Torin booked a seat on Eurowings flight EW23
 
-// 2. with event listener
-lufthansa.planes = 300;
-lufthansa.buyPlane = function () {
-  console.log(this);
-  this.planes++;
-  console.log(this.planes);
+// // 2. with event listener
+// lufthansa.planes = 300;
+// lufthansa.buyPlane = function () {
+//   console.log(this);
+//   this.planes++;
+//   console.log(this.planes);
+// };
+// document.querySelector('.buy').addEventListener('click', lufthansa.buyPlane);
+// // <button class="buy"> Buy new plane </button>
+// // NaN
+// document
+//   .querySelector('.buy')
+//   .addEventListener('click', lufthansa.buyPlane.bind(lufthansa));
+// // {airline: 'Lufthansa', iataCode: 'LH', bookings: Array(2), planes: 300, book: ƒ, …}
+// // 301
+
+// // Partial application (Bind no this general function)
+
+// const addTax = (rate, value) => value + value * rate;
+// console.log(addTax(0.1, 100));
+// console.log(addTax.this); // undefine
+// const addVAT = addTax.bind(null, 0.23);
+// // rewrite with function returen function
+// const addTax2 = function (rate) {
+//   return function (value) {
+//     return value + value * rate;
+//   };
+// };
+// // DONE
+// const addVAT2 = addTax2(0.5);
+// addVAT2(200); // 300
+
+/* #################################
+Challange 1
+####################################*/
+const poll = {
+  question: 'What is your favourite programming language?',
+  options: ['0: JavaScript', '1: Python', '2: Rust', '3:C++'],
+  // This generates [0, 0, 0, 0]. More in the next section!
+  answers: new Array(4).fill(0),
 };
-document.querySelector('.buy').addEventListener('click', lufthansa.buyPlane);
-// <button class="buy"> Buy new plane </button>
-// NaN
-document
-  .querySelector('.buy')
-  .addEventListener('click', lufthansa.buyPlane.bind(lufthansa));
-// {airline: 'Lufthansa', iataCode: 'LH', bookings: Array(2), planes: 300, book: ƒ, …}
-// 301
-
-// Partial application (Bind no this general function)
-
-const addTax = (rate, value) => value + value * rate;
-console.log(addTax(0.1, 100));
-console.log(addTax.this); // undefine
-const addVAT = addTax.bind(null, 0.23);
-// rewrite with function returen function
-const addTax2 = function (rate) {
-  return function (value) {
-    return value + value * rate;
-  };
+// 1.1 , 1.2 prompt answer and push to answers
+poll.registerNewAnswer = function () {
+  let answer = prompt(
+    `What is your favourite programming language?\n${this.options.join('\n')}`
+  );
+  if ([0, 1, 2, 3].includes(Number(answer))) {
+    this.answers[answer]++;
+  }
 };
-// DONE
-const addVAT2 = addTax2(0.5);
-addVAT2(200); // 300
+poll.registerNewAnswer();
+console.log(poll);
