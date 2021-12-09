2fc36f4e10364d556623327d5147fc6517a356b7
Torin Zhou <torinzhou@gmail.com>
2021-12-09 15:32:39 +0800

    12.9

--- 10-Functions/starter/script.js
+++ 10-Functions/starter/script.js
@@ -175,15 +175,25 @@ const poll = {
   options: ['0: JavaScript', '1: Python', '2: Rust', '3:C++'],
   // This generates [0, 0, 0, 0]. More in the next section!
   answers: new Array(4).fill(0),
+  // 1.1 , 1.2 prompt answer and push to answers
+  // 2 Call this method whenever the user clicks the "Answer poll" button.
+  registerNewAnswer() {
+    let answer = prompt(`${this.question}?\n${this.options.join('\n')}`);
+    if ([0, 1, 2, 3].includes(Number(answer))) {
+      this.answers[answer]++;
+    }
+    this.displayResults.bind(poll)();
+  },
+  // 3.
+  displayResults(type = 'string') {
+    type = prompt('Which type of reuslt would you like to check? ');
+    if (type === 'string' || type === '')
+      return console.log(`Poll results are ${this.answers}`);
+    if (type === 'array') return console.log(this.answers);
+  },
 };
-// 1.1 , 1.2 prompt answer and push to answers
-poll.registerNewAnswer = function () {
-  let answer = prompt(
-    `What is your favourite programming language?\n${this.options.join('\n')}`
-  );
-  if ([0, 1, 2, 3].includes(Number(answer))) {
-    this.answers[answer]++;
-  }
-};
-poll.registerNewAnswer();
-console.log(poll);
+document
+  .querySelector('.poll')
+  .addEventListener('click', poll.registerNewAnswer.bind(poll));
+
+// 3. Create a method 'displayResults' which displays the poll results. The method takes a string as an input (called 'type'), which can be either 'string' or 'array'. If type is 'array', simply display the results array as it is, using console.log(). This should be the default option. If type is 'string', display a string like "Poll results are 13, 2, 4, 1".
