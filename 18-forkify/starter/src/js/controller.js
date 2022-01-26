b4c57cafb4e40d529af9ecd1a9a67c07ae13c6d2
Torin Zhou <torinzhou@gmail.com>
2022-01-26 15:19:35 +0800

    1.26

--- 18-forkify/starter/src/js/controller.js
+++ 18-forkify/starter/src/js/controller.js
@@ -1,9 +1,8 @@
 import * as model from './model.js';
 import recipeView from './views/recipeView.js';
+import searchView from './views/searchView.js';
 import 'core-js/stable';
 import 'regenerator-runtime/runtime';
-
-const recipeContainer = document.querySelector('.recipe');
 
 // https://forkify-api.herokuapp.com/v2
 
@@ -26,8 +25,23 @@ const controlRecipes = async function () {
     recipeView.renderError();
   }
 };
+const controlSearchResults = async function () {
+  try {
+    // 1) Get search quer y
+    const query = searchView.getQuery();
+    if (!query) return;
 
+    // 2) Load search results
+    await model.loadSearchResult(query);
+
+    // 3) Render results
+    console.log(model.state.search.results);
+  } catch (err) {
+    console.log(err);
+  }
+};
 const init = function () {
   recipeView.addHandlerRender(controlRecipes);
+  searchView.addHandlerSearch(controlSearchResults);
 };
 init();
